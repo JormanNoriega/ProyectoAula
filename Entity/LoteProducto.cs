c055faf2832d9e1a35a0951d7ab161bc1ff06ca5
JormanNoriega <jormandavidnoriegadavila@gmail.com>
2023-10-09 21:12:35 -0500

    entidades

---
+++ Entity/LoteProducto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    internal class LoteProducto
+    {
+        public int codLote { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        private double precioCompra { get; set; }
+        private double precioVenta { get; set; }
+        public int cantidad { get; set; }
+
+        public LoteProducto()
+        {
+        }
+
+        public LoteProducto(int codLote, DateTime fechaVencimiento, double precioCompra, double precioVenta, int cantidad)
+        {
+            this.codLote = codLote;
+            FechaVencimiento = fechaVencimiento;
+            this.precioCompra = precioCompra;
+            this.precioVenta = precioVenta;
+            this.cantidad = cantidad;
+        }
+    }
+}
---
+++ Entity/Producto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Producto
+    {
+        private int codProducto {  get; set;}
+        private string nombProducto { get; set;}
+        private string laboratorio { get; set;}
+        private string cartegoria { get; set;}
+        private string publicoDirigido { get; set;}
+        private string descripcion { get; set;}
+        private List<LoteProducto> lotes {get; set;}
+
+        public Producto(int codProducto, string nombProducto, string laboratorio, string cartegoria, string publicoDirigido, string descripcion)
+        {
+            this.codProducto = codProducto;
+            this.nombProducto = nombProducto;
+            this.laboratorio = laboratorio;
+            this.cartegoria = cartegoria;
+            this.publicoDirigido = publicoDirigido;
+            this.descripcion = descripcion;
+            this.lotes = new List<LoteProducto>();
+        }
+    }
+}
